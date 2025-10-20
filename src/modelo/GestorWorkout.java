6809239f11628c975f9d47dfa58c2ec90ea21347
Anaitz Greño <anaitz.grenopa@elorrieta-errekamari.com>
2025-10-20 14:23:22 +0200

    gestor

--- src/modelo/GestorWorkout.java
+++ src/modelo/GestorWorkout.java
@@ -2,10 +2,14 @@ package modelo;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
+import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
@@ -30,7 +34,7 @@ public class GestorWorkout {
 		return works;
 
 
-		
+
 		
 		
 	}
