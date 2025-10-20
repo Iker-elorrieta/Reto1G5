e2297f2b50228c87899998149a721a740446d279
Anaitz Greño <anaitz.grenopa@elorrieta-errekamari.com>
2025-10-20 10:34:29 +0200

    wokrout en general

---
+++ src/modelo/Workout.java
@@ -0,0 +1,46 @@
+package modelo;
+
+public class Workout {
+	private String nombre;
+	private String video;
+	private int nivel;
+	private int numEjers;
+	
+	public Workout() {
+		this.nombre="";
+		this.video="";
+		this.nivel=0;
+		
+	}
+	public Workout( String nombre, String video, int nivel, int numEjers) {
+		this.nombre=nombre;
+		this.video=video;
+		this.nivel=nivel;
+		this.numEjers=numEjers;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getVideo() {
+		return video;
+	}
+	public void setVideo(String video) {
+		this.video = video;
+	}
+	public int getNivel() {
+		return nivel;
+	}
+	public void setNivel(int nivel) {
+		this.nivel = nivel;
+	}
+	public int getNumEjers() {
+		return numEjers;
+	}
+	public void setNumEjers(int numEjers) {
+		this.numEjers = numEjers;
+	}
+	
+}
---
+++ src/vista/VentanaWorkouts.java
@@ -0,0 +1,52 @@
+package vista;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class VentanaWorkouts extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaWorkouts frame = new VentanaWorkouts();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaWorkouts() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 937, 623);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(31, 112, 863, 414);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+
+	}
+}
